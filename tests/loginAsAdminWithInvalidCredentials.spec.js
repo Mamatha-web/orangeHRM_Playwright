27d2b47262bcb062fffc4034ff3bafdec311f3c8
Mamatha-web <mamathasundar2323@gmail.com>
2026-04-06 15:44:28 +0530

    second commit

--- tests/loginAsAdminWithInvalidCredentials.spec.js
+++ tests/loginAsAdminWithInvalidCredentials.spec.js
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test'
 import { LoginPage } from '../pom/login.page'
 import jData from '../testData/configData_invalid.json'
 
-test('loginToApp', async({page})=>{
+test('loginToAppWithInvalid', async({page})=>{
     let login = new LoginPage(page)
     await login.navigate(jData.url)
     await login.login(jData.username,jData.password)
